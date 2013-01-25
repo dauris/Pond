16e69dba08c097824a30e731006979bfb4b80b7d
Guest User <Guest@Mac110-Simic.local>
2013-01-25 11:28:19 -0500

    new and improved (possibly)

---
+++ Pond/Pond/PrototypePond.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pond
+//
+//  Created by Guest User on 1/25/13.
+//  Copyright (c) 2013 Tech Guyz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
+
+
+@interface PrototypePond : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
